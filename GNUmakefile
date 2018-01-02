18d76da24e4e10495017e8c5d6a599aaeecfcb41
Joe Rahmeh <joseph.rahmeh@wdc.com>
2018-01-02 13:51:19 -0800

    Now using boost to parse command line options.

--- GNUmakefile
+++ GNUmakefile
@@ -1,19 +1,22 @@
 OFLAGS := -O3
 IFLAGS := -I/usr/local/include
+LFLAGS := /usr/local/lib/libboost_program_options.a
+
+COMP := $(CXX) -std=gnu++11 $(OFLAGS) $(IFLAGS)
 
 %.o:  %.cpp
-	$(CXX) -std=gnu++14 $(OFLAGS) $(IFLAGS) -c -o $@ $^
+	$(COMP) -c -o $@ $^
 
 OBJ := CsRegs.o Core.o sim.o Inst.o Memory.o
 
 sim: $(OBJ)
-	$(CXX) -std=gnu++14 $(OFLAGS) -o $@ $^
+	$(COMP) $@ $^
 
 gen16codes: gen16codes.o CsRegs.o Core.o Inst.o Memory.o
-	$(CXX) -std=gnu++14 $(OFLAGS) -o $@ $^
+	$(COMP) -o $@ $^
 
 gen16codesb: gen16codesb.o CsRegs.o Core.o Inst.o Memory.o
-	$(CXX) -std=gnu++14 $(OFLAGS) -o $@ $^
+	$(CXX) -o $@ $^ $(LFLAGS)
 
 clean:
 	$(RM) sim gen16codes $(OBJ) gen16codes.o
--- gen16codesb.cpp
+++ gen16codesb.cpp
@@ -1,35 +1,72 @@
 #include <stdio.h>
+#include <iostream>
+#include <boost/program_options.hpp>
 #include "Core.hpp"
 #include "Inst.hpp"
 
 using namespace WdRiscv;
 
 
+// Print on the standard output in hexadecimal notation the given
+// 16-bit instruction code followed by the equivalent 32-bit instruction
+// code.  If verbose is true, also print the assembly code of both
+// 16 and 32 bit instructions.
 static void
-printExpanded(Core<uint32_t>& core, uint16_t inst16)
+printExpanded(Core<uint32_t>& core, uint16_t inst16, bool verbose)
 {
   uint32_t inst32 = 0;
-  if (core.expandInst(inst16, inst32))
+  core.expandInst(inst16, inst32);
+
+  printf("%04x %08x", inst16, inst32);
+  if (verbose)
     {
-#if 0
       std::string asm16, asm32;
-      core.disassembleInst(inst16, asm16);
-      printf("%s", asm16.c_str());
-      core.disassembleInst(inst32, asm32);
-      printf("   %s\n", asm32.c_str());
-#endif
+      core.disassembleInst16(inst16, asm16);
+      core.disassembleInst32(inst32, asm32);
+      printf("  %s  %s", asm16.c_str(), asm32.c_str());
     }
-  printf("%04x %08x\n", inst16, inst32);
+  printf("\n");
 }
 
 
+// Generate on the standard output all 16-bit instruction code and
+// their equivalent 32-bit instruction codes.
 int
 main(int argc, char* argv[])
 {
+  bool verbose = false;
+
+  try
+    {
+      // Define command line options.
+      namespace po = boost::program_options;
+      po::options_description desc("options");
+      desc.add_options()
+	("help", "Generate all compressed instrution codes and their "
+	 "equivalent 32-bit instruction codes.")
+	("verbose", "Generate assemble forms of compressed and expanded instructions");
+
+      po::variables_map varMap;
+      po::store(po::parse_command_line(argc, argv, desc), varMap);
+      verbose = varMap.count("verbose") > 0;
+      if (varMap.count("help"))
+	{
+	  std::cout << "Generate all riscv compressed instruction codes and\n"
+		    << "their equivalent 32-bit codes\n";
+	  return 0;
+	}
+    }
+  catch(std::exception& exp)
+    {
+      std::cerr << "Failed to parse command line args: " << exp.what() << '\n';
+      return 1;
+    }
+
+
   Core<uint32_t> core(1024, 32);
 
   for (unsigned inst16 = 0; inst16 <= 0xffff; ++inst16)
-    printExpanded(core, inst16);
+    printExpanded(core, inst16, verbose);
 
   return 0;
 }
