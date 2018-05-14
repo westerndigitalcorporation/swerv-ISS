1dbcac7d7ca9e6716010f6a9b799fe04a11b0295
Joseph Rahmeh <joseph.rahmeh@wdc.com>
2018-05-14 17:41:32 -0500

    Now at version 1.41

--- whisper.cpp
+++ whisper.cpp
@@ -2053,7 +2053,7 @@ main(int argc, char* argv[])
     return 1;
 
   unsigned version = 1;
-  unsigned subversion = 40;
+  unsigned subversion = 41;
 
   if (args.version)
     std::cout << "Version " << version << "." << subversion << " compiled on "
