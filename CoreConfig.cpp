fdaa5fe1455a265d865037c7c749e9ae5e714ff6
Joseph Rahmeh <joseph.rahmeh@wdc.com>
2018-10-16 15:49:29 -0500

    Factored configuration file code.

---
+++ CoreConfig.cpp
@@ -0,0 +1,516 @@
+#include <nlohmann/json.hpp>
+#include <fstream>
+#include <iostream>
+#include "CoreConfig.hpp"
+#include "Core.hpp"
+
+
+using namespace WdRiscv;
+
+
+CoreConfig::CoreConfig()
+{
+  config_ = new nlohmann::json();
+}
+
+
+CoreConfig::~CoreConfig()
+{
+  delete config_;
+  config_ = nullptr;
+}
+
+
+bool
+CoreConfig::loadConfigFile(const std::string& filePath)
+{
+  std::ifstream ifs(filePath);
+  if (not ifs.good())
+    {
+      std::cerr << "Failed to open config file '" << filePath << "'\n";
+      return false;
+    }
+
+  try
+    {
+      ifs >> *config_;
+    }
+  catch (std::exception& e)
+    {
+      std::cerr << e.what() << "\n";
+      return false;
+    }
+  catch (...)
+    {
+      std::cerr << "Caught unknown exception while parsing "
+		<< " config file '" << filePath << "'\n";
+      return false;
+    }
+
+  return true;
+}
+
+
+namespace WdRiscv
+{
+  
+  /// Convert given json value to an unsigned integer honoring any
+  /// hexadecimal prefix (0x) if any.
+  uint64_t
+  getJsonUnsigned(const std::string& tag, const nlohmann::json& js)
+  {
+    if (js.is_number())
+      return js.get<unsigned>();
+    if (js.is_string())
+      {
+	char *end = nullptr;
+	std::string str = js.get<std::string>();
+	uint64_t x = strtoull(str.c_str(), &end, 0);
+	if (end and *end)
+	  std::cerr << "Invalid config file value for '" << tag << "': "
+		    << str << '\n';
+	return x;
+      }
+    std::cerr << "Config file entry '" << tag << "' must contain a number\n";
+    return 0;
+  }
+
+
+  /// Convert given json array value to an vector of unsigned integers
+  /// honoring any hexadecimal prefix (0x) if any.
+  std::vector<uint64_t>
+  getJsonUnsignedVec(const std::string& tag, const nlohmann::json& js)
+  {
+    std::vector<uint64_t> vec;
+
+    if (not js.is_array())
+      {
+	std::cerr << "Invalid config file value for '" << tag << "'"
+		  << " -- expecting array of numbers\n";
+	return vec;
+      }
+
+    for (const auto& item :js)
+      {
+	if (item.is_number())
+	  vec.push_back(item.get<unsigned>());
+	else if (item.is_string())
+	  {
+	    char *end = nullptr;
+	    std::string str = item.get<std::string>();
+	    uint64_t x = strtoull(str.c_str(), &end, 0);
+	    if (end and *end)
+	      std::cerr << "Invalid config file value for '" << tag << "': "
+			<< str << '\n';
+	    else
+	      vec.push_back(x);
+	  }
+	else
+	  std::cerr << "Invalid config file value for '" << tag << "'"
+		    << " -- expecting array of number\n";
+      }
+
+    return vec;
+  }
+
+
+  /// Convert given json value to a boolean.
+  bool
+  getJsonBoolean(const std::string& tag, const nlohmann::json& js)
+  {
+    if (js.is_boolean())
+      return js.get<bool>();
+    if (js.is_number())
+      return js.get<unsigned>();
+    if (js.is_string())
+      {
+	std::string str = js.get<std::string>();
+	if (str == "0" or str == "false" or str == "False")
+	  return false;
+	if (str == "1" or str == "true" or str == "True")
+	  return true;
+	std::cerr << "Invalid config file value for '" << tag << "': "
+		  << str << '\n';
+	return false;
+      }
+    std::cerr << "Config file entry '" << tag << "' must contain a bool\n";
+    return false;
+  }
+
+}
+
+
+template <typename URV>
+static
+bool
+applyCsrConfig(Core<URV>& core, const nlohmann::json& config)
+{
+  if (not config.count("csr"))
+    return true;  // Nothing to apply
+
+  const auto& csrs = config.at("csr");
+  if (not csrs.is_object())
+    {
+      std::cerr << "Invalid csr entry in config file (expecting an object)\n";
+      return false;
+    }
+
+  unsigned errors = 0;
+  for (auto it = csrs.begin(); it != csrs.end(); ++it)
+    {
+      const std::string& csrName = it.key();
+      const auto& conf = it.value();
+
+      bool exists = true;
+      if (conf.count("exists"))
+	exists = getJsonBoolean(csrName + ".bool", conf.at("exists"));
+
+      URV reset = 0, mask = 0, pokeMask = 0;
+
+      if (exists)   // Must have reset and mask
+	{
+	  bool ok = true;
+	  for (const auto& tag : {"reset", "mask"})
+	    if (not conf.count(tag))
+	      {
+		std::cerr << "CSR '" << csrName << "' has no '" << tag
+			  << "' entry\n";
+		ok = false;
+	      }
+	  if (not ok)
+	    {
+	      errors++;
+	      continue;
+	    }
+
+	  reset = getJsonUnsigned(csrName + ".reset", conf.at("reset"));
+	  mask = getJsonUnsigned(csrName + ".mask", conf.at("mask"));
+	  pokeMask = mask;
+	  if (conf.count("poke_mask"))
+	    pokeMask = getJsonUnsigned(csrName + ".poke_mask",
+				       conf.at("poke_mask"));
+	}
+
+      if (not core.configCsr(csrName, exists, reset, mask, pokeMask))
+	{
+	  std::cerr << "Invalid CSR (" << csrName
+		    << ") in configuration file.\n";
+	  errors++;
+	}
+    }
+
+  return errors == 0;
+}
+
+
+template <typename URV>
+static
+bool
+applyPicConfig(Core<URV>& core, const nlohmann::json& config)
+{
+  if (not config.count("pic"))
+    return true;  // Nothing to apply.
+
+  const auto& pic = config.at("pic");
+  bool badPic = false;
+  for (const auto& tag : { "region", "size", "offset", "mpiccfg_offset",
+	"meipl_offset", "meip_offset", "meie_offset", "meigwctrl_offset",
+	"meigwclr_offset", "total_int", "int_words" } )
+    {
+      if (not pic.count(tag))
+	{
+	  std::cerr << "Missing '" << tag << "' entry in "
+		    << "config file pic section\n";
+	  badPic = true;
+	}
+    }
+
+  if (badPic)
+    ;
+
+  // Define pic region.
+  uint64_t region = getJsonUnsigned("region", pic.at("region"));
+  uint64_t size = getJsonUnsigned("size", pic.at("size"));
+  uint64_t regionOffset = getJsonUnsigned("offset", pic.at("offset"));
+  if (not core.defineMemoryMappedRegisterRegion(region, regionOffset, size))
+    return false;
+
+  // Define the memory mapped registers.
+  uint64_t smax = getJsonUnsigned("pic.total_int", pic.at("total_int"));
+  uint64_t xmax = getJsonUnsigned("pic.int_words", pic.at("int_words"));
+
+  unsigned errors = 0;
+
+  // Start by giving all registers in region a mask of zero.
+  size_t possibleRegCount = size / 4;
+  for (size_t ix = 0; ix < possibleRegCount; ++ix)
+    core.defineMemoryMappedRegisterWriteMask(region, regionOffset, 0, ix, 0);
+
+  std::vector<std::string> names = { "mpiccfg_offset", "meipl_offset",
+				     "meip_offset", "meie_offset",
+				     "meigwctrl_offset", "meigwclr_offset" };
+
+  // These should be in the config file. The mask for meigwclr is zero
+  // because the state is always zero.
+  std::vector<uint32_t> masks = { 1, 0xf, 0, 1, 3, 0 };
+  std::vector<size_t> counts = { 1, smax, xmax, smax, smax, smax };
+
+  // meipl, meie, meigwctrl and meigwclr indexing start at 1 (instead
+  // of 0): adjust
+  std::vector<size_t> adjust = { 0, 4, 0, 4, 4, 4 };
+
+  for (size_t i = 0; i < names.size(); ++i)
+    {
+      auto mask = masks.at(i);
+      const auto& name = names.at(i);
+      auto count = counts.at(i);
+
+      if (not pic.count(name))
+	continue;  // Should be an error.
+
+      uint64_t registerOffset = getJsonUnsigned(("pic." + name), pic.at(name));
+      registerOffset += adjust.at(i);
+      for (size_t regIx = 0; regIx < count; ++regIx)
+	if (not core.defineMemoryMappedRegisterWriteMask(region, regionOffset,
+							 registerOffset, regIx,
+							 mask))
+	  errors++;
+    }
+
+  return errors == 0;
+}
+
+
+template <typename URV>
+static
+bool
+applyTriggerConfig(Core<URV>& core, const nlohmann::json& config)
+{
+  if (not config.count("triggers"))
+    return true;  // Nothing to apply
+
+  const auto& triggers = config.at("triggers");
+  if (not triggers.is_array())
+    {
+      std::cerr << "Invalid triggers entry in config file (expecting an array)\n";
+      return false;
+    }
+
+  unsigned errors = 0;
+  unsigned ix = 0;
+  for (auto it = triggers.begin(); it != triggers.end(); ++it, ++ix)
+    {
+      const auto& trig = *it;
+      std::string name = std::string("trigger") + std::to_string(ix);
+      if (not trig.is_object())
+	{
+	  std::cerr << "Invalid trigger in config file triggers array "
+		    << "(expecting an object at index " << std::dec << ix << ")\n";
+	  ++errors;
+	  break;
+	}
+      bool ok = true;
+      for (const auto& tag : {"reset", "mask", "poke_mask"})
+	if (not trig.count(tag))
+	  {
+	    std::cerr << "Trigger " << name << " has no '" << tag << "' entry\n";
+	    ok = false;
+	  }
+      if (not ok)
+	{
+	  errors++;
+	  continue;
+	}
+      auto resets = getJsonUnsignedVec(name + ".reset", trig.at("reset"));
+      auto masks = getJsonUnsignedVec(name + ".mask", trig.at("mask"));
+      auto pokeMasks = getJsonUnsignedVec(name + ".poke_mask", trig.at("poke_mask"));
+
+      if (resets.size() != 3)
+	{
+	  std::cerr << "Trigger " << name << ": Bad item count (" << resets.size()
+		    << ") in 'reset' field. Expecting 3.\n";
+	  ok = false;
+	}
+
+      if (masks.size() != 3)
+	{
+	  std::cerr << "Trigger " << name << ": Bad item count (" << masks.size()
+		    << ") in 'mask' field. Expecting 3.\n";
+	  ok = false;
+	}
+
+      if (pokeMasks.size() != 3)
+	{
+	  std::cerr << "Trigger " << name << ": Bad item count (" << pokeMasks.size()
+		    << ") in 'poke_mask' field. Expecting 3.\n";
+	  ok = false;
+	}
+
+      if (not ok)
+	{
+	  errors++;
+	  continue;
+	}
+      if (not core.configTrigger(ix, resets.at(0), resets.at(1), resets.at(2),
+				 masks.at(0), masks.at(1), masks.at(2),
+				 pokeMasks.at(0), pokeMasks.at(1), pokeMasks.at(2)))
+	{
+	  std::cerr << "Failed to configure trigger " << std::dec << ix << '\n';
+	  ++errors;
+	}
+    }
+
+  return errors == 0;
+}
+
+
+template<typename URV>
+bool
+CoreConfig::applyConfig(Core<URV>& core) const
+{
+  // Define PC value after reset.
+  if (config_ -> count("reset_vec"))
+    {
+      URV resetPc = getJsonUnsigned("reset_vec", config_ -> at("reset_vec"));
+      core.defineResetPc(resetPc);
+    }
+
+  // Define non-maskable-interrupt pc
+  if (config_ -> count("nmi_vec"))
+    {
+      URV nmiPc = getJsonUnsigned("nmi_vec", config_ -> at("nmi_vec"));
+      core.defineNmiPc(nmiPc);
+    }
+
+  if (config_ -> count("store_error_rollback"))
+    {
+      bool ser = getJsonBoolean("store_error_rollback",
+				config_ -> at("store_error_rollback"));
+      core.enableStoreErrorRollback(ser);
+    }
+
+  if (config_ -> count("load_error_rollback"))
+    {
+      bool ler = getJsonBoolean("load_error_rollback",
+				config_ -> at("load_error_rollback"));
+      core.enableLoadErrorRollback(ler);
+    }
+
+  if (config_ -> count("memmap"))
+    {
+      const auto& memmap = config_ -> at("memmap");
+      if (memmap.count("consoleio"))
+	{
+	  URV io = getJsonUnsigned("memmap.consoleio", memmap.at("consoleio"));
+	  core.setConsoleIo(io);
+	}
+    }
+
+  if (config_ -> count("even_odd_trigger_chains"))
+    {
+      bool chainPairs = getJsonBoolean("even_odd_trigger_chains",
+				       config_ -> at("even_odd_trigger_chains"));
+      core.configEvenOddTriggerChaining(chainPairs);
+    }
+
+  unsigned errors = 0;
+
+  if (config_ -> count("iccm"))
+    {
+      const auto& iccm = config_ -> at("iccm");
+      if (iccm.count("region") and iccm.count("size") and iccm.count("offset"))
+	{
+	  size_t region = getJsonUnsigned("iccm.region", iccm.at("region"));
+	  size_t size   = getJsonUnsigned("iccm.size",   iccm.at("size"));
+	  size_t offset = getJsonUnsigned("iccm.offset", iccm.at("offset"));
+	  if (not core.defineIccm(region, offset, size))
+	    errors++;
+	}
+      else
+	{
+	  std::cerr << "The iccm entry in the configuration file must contain "
+		    << "a region, offset and a size entry.\n";
+	  errors++;
+	}
+    }
+
+  if (config_ -> count("dccm"))
+    {
+      const auto& dccm = config_ -> at("dccm");
+      if (dccm.count("region") and dccm.count("size") and dccm.count("offset"))
+	{
+	  size_t region = getJsonUnsigned("dccm.region", dccm.at("region"));
+	  size_t size   = getJsonUnsigned("dccm.size",   dccm.at("size"));
+	  size_t offset = getJsonUnsigned("dccm.offset", dccm.at("offset"));
+	  if (not core.defineDccm(region, offset, size))
+	    errors++;
+	}
+      else
+	{
+	  std::cerr << "The dccm entry in the configuration file must contain "
+		    << "a region, offset and a size entry.\n";
+	  errors++;
+	}
+    }
+
+  std::string tag = "num_mmode_perf_regs";
+  if (config_ -> count(tag))
+    {
+      unsigned count = getJsonUnsigned(tag, config_ -> at(tag));
+      if (not core.configMachineModePerfCounters(count))
+	errors++;
+    }
+
+  tag = "max_mmode_perf_event";
+  if (config_ -> count(tag))
+    {
+      unsigned maxId = getJsonUnsigned(tag, config_ -> at(tag));
+      core.configMachineModeMaxPerfEvent(maxId);
+    }
+
+  if (not applyCsrConfig(core, *config_))
+    errors++;
+
+  if (not applyPicConfig(core, *config_))
+    errors++;
+
+  if (not applyTriggerConfig(core, *config_))
+    errors++;
+
+  core.finishMemoryConfig();
+
+  return errors == 0;
+}
+
+
+bool
+CoreConfig::getXlen(unsigned& xlen) const
+{
+  if (config_ -> count("xlen"))
+    {
+      xlen = getJsonUnsigned("xlen", config_ -> at("xlen"));
+      return true;
+    }
+  return false;
+}
+
+
+void
+CoreConfig::clear()
+{
+  config_ -> clear();
+}
+
+
+bool
+CoreConfig::apply(CoreConfig& conf, Core<uint32_t>& core)
+{
+  return conf.applyConfig(core);
+}
+
+
+bool
+CoreConfig::apply(CoreConfig& conf, Core<uint64_t>& core)
+{
+  return conf.applyConfig(core);
+}
---
+++ CoreConfig.hpp
@@ -0,0 +1,61 @@
+#pragma once
+
+#include <string>
+#include <nlohmann/json_fwd.hpp>
+
+
+namespace WdRiscv
+{
+
+  template <typename URV>
+  class Core;
+
+
+  /// Manage loading of conficuration file and applying it to a core.
+  class CoreConfig
+  {
+  public:
+
+    /// Constructor.
+    CoreConfig();
+
+    /// Destructor.
+    ~CoreConfig();
+
+    /// Load given configuration file (JOSN file) into this object.
+    /// Return true on success and false if file cannot be opened or if the file
+    /// does not contain a valid JSON object.
+    bool loadConfigFile(const std::string& filePath);
+
+    /// Apply the configurations in this object (as loaded by
+    /// loadConfigFile) to the given core. Return true on success and
+    /// false on failure. URV stands for unsigned register type and is
+    /// the type associated with the integer registers of a core. Use
+    /// uint32_t for 32-bit cores and uint64_t for 64-bit cores.
+    template<typename URV>
+    bool applyConfig(Core<URV>&) const;
+
+    /// Set xeln to the register width configuration held in this
+    /// object returning true on success and false if this object does
+    /// not contain a register width (xlen) configuration.
+    bool getXlen(unsigned& registerWidth) const;
+
+    /// Clear (make emtpy) the set of configurations held in this object.
+    void clear();
+
+  private:
+
+    /// Force instantiation of applyConfig(Core<uint32_t>).
+    static bool apply(CoreConfig&, Core<uint32_t>&);
+
+    /// Force instantiation of applyConfig(Core<uint64_t>).
+    static bool apply(CoreConfig&, Core<uint64_t>&);
+
+    CoreConfig(const CoreConfig&) = delete;
+    void operator= (const CoreConfig&) = delete;
+
+    nlohmann::json* config_;
+  };
+
+}
+
