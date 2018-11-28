b6e87808b4de9c98955c31d3d54604553ce7b2b0
Joseph Rahmeh <joseph.rahmeh@wdc.com>
2018-11-28 14:33:27 -0800

    Put copyright sentence on its own line.

--- PerfRegs.cpp
+++ PerfRegs.cpp
@@ -1,6 +1,6 @@
 //
-// SPDX-License-Identifier: GPL-3.0-or-later Copyright 2018 Western Digital
-// Corporation or its affiliates.
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright 2018 Western Digital Corporation or its affiliates.
 // 
 // This program is free software: you can redistribute it and/or modify it
 // under the terms of the GNU General Public License as published by the Free
