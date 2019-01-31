bd1fbf285070aa8045b3279cab85cd5e5fd391ed
Joseph Rahmeh <joseph.rahmeh@wdc.com>
2019-01-31 07:49:15 -0800

    Instructions c.addiw and c.sdsp now have their own unique ids.

--- InstId.hpp
+++ InstId.hpp
@@ -88,12 +88,13 @@ namespace WdRiscv
       c_addi4spn, c_fld, c_lq, c_lw, c_flw, c_ld,
       c_fsd, c_sq, c_sw, c_fsw, c_sd,      
 
-      c_addi, c_jal, c_addiw = c_jal, c_li, c_addi16sp, c_lui, c_srli,
+      c_addi, c_jal, c_li, c_addi16sp, c_lui, c_srli,
       c_srli64, c_srai, c_srai64, c_andi, c_sub, c_xor,
       c_or, c_and, c_subw, c_addw, c_j, c_beqz, c_bnez,
       c_slli, c_slli64, c_fldsp, c_lwsp, c_flwsp, c_ldsp, c_jr, c_mv,
-      c_ebreak, c_jalr, c_add, c_fsdsp, c_swsp, c_fswsp, c_sdsp = c_fswsp,
+      c_ebreak, c_jalr, c_add, c_fsdsp, c_swsp, c_fswsp,
+      c_addiw, c_sdsp,
 
-      maxId = c_fswsp
+      maxId = c_sdsp
     };
 }
