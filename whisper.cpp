60d8a437aeaa8ec73aaf5e49ce9f6eb27a522318
Joseph Rahmeh <joseph.rahmeh@wdc.com>
2019-02-16 08:34:11 -0800

    Added time-stamp field to WhisperMessage

--- whisper.cpp
+++ whisper.cpp
@@ -844,7 +844,7 @@ main(int argc, char* argv[])
     return 1;
 
   unsigned version = 1;
-  unsigned subversion = 265;
+  unsigned subversion = 266;
   if (args.version)
     std::cout << "Version " << version << "." << subversion << " compiled on "
 	      << __DATE__ << " at " << __TIME__ << '\n';
