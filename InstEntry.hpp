f5c02467cf2e217824b5116b7f615edd26f5fc35
Joseph Rahmeh <joseph.rahmeh@wdc.com>
2019-06-18 09:36:17 -0700

    Fixed return types of InstEntry::storeSize/loadSize.

--- InstEntry.hpp
+++ InstEntry.hpp
@@ -208,12 +208,12 @@ namespace WdRiscv
 
     /// Return the data size in bytes of a load instruction. Return
     /// zero for a non-load instruction.
-    bool loadSize() const
+    unsigned loadSize() const
     { return ldSize_; }
 
     /// Return the data size in bytes of a store instruction. Return
     /// zero for a non-store instruction.
-    bool storeSize() const
+    unsigned storeSize() const
     { return stSize_; }
 
   protected:
