11ff47ab386aca610cd0ff615d01d3c62ee80186
Joseph Rahmeh <joseph.rahmeh@wdc.com>
2019-07-30 08:43:27 -0700

    Bump version to 1.361.

--- whisper.cpp
+++ whisper.cpp
@@ -993,7 +993,7 @@ main(int argc, char* argv[])
     return 1;
 
   unsigned version = 1;
-  unsigned subversion = 360;
+  unsigned subversion = 361;
   if (args.version)
     std::cout << "Version " << version << "." << subversion << " compiled on "
 	      << __DATE__ << " at " << __TIME__ << '\n';
