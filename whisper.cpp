3c0e6e646eeeeb549b999c259aa5f7a1edcd2753
Joseph Rahmeh <joseph.rahmeh@wdc.com>
2019-09-26 09:02:07 -0700

    Update stack checker to allow load from top of stack.

--- whisper.cpp
+++ whisper.cpp
@@ -194,7 +194,7 @@ void
 printVersion()
 {
   unsigned version = 1;
-  unsigned subversion = 412;
+  unsigned subversion = 414;
   std::cout << "Version " << version << "." << subversion << " compiled on "
 	    << __DATE__ << " at " << __TIME__ << '\n';
 }
